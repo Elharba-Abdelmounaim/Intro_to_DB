b6e455680fbe814108f3829b28b71492a52d5111
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-07-28 03:43:02 +0100

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS authors (
 CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    author_id INT,
-    price DECIMAL(10, 2),
-    quantity INT,
+    author_id INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT DEFAULT 0,
     FOREIGN KEY (author_id) REFERENCES authors(id)
 );
 
@@ -25,17 +25,17 @@ CREATE TABLE IF NOT EXISTS customers (
 -- Create Orders Table
 CREATE TABLE IF NOT EXISTS orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
--- Create Order_Details Table
+-- Create Order Details Table
 CREATE TABLE IF NOT EXISTS order_details (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity INT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id),
     FOREIGN KEY (book_id) REFERENCES books(id)
 );
